40d88c44eba5a09e825589694b130d05c687b9ff
Andy Gómez Ordinola <45979588+Koato@users.noreply.github.com>
2020-05-05 18:28:15 -0500

    cambios en la vista del login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,12 +1,56 @@
 import React, {Component} from 'react';
-import Tarjeta from "../../components/Tarjeta/Tarjeta";
+import "./Login.css";
+import {Card} from 'primereact/card';
+import {InputText} from "primereact/inputtext";
+import {Captcha} from 'primereact/captcha';
+import {Button} from 'primereact/button';
 
 class Login extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            alias: '',
+            clave: ''
+        }
+    }
+
+    showResponse(response) {
+        console.log("Validacion Captcha respondida");
+    }
+
     render(){
         return(
-            <div>
-                <h1>Hola</h1>
-                <Tarjeta />
+            <div className="padre content-section implementation">
+                <Card title="Acceso al sistema" subTitle="Ingrese sus credenciales" className="cardLogin ui-card-shadow">
+                    <form onSubmit>
+                        <div className="content-section implementation">
+                            <div className="p-col-12 p-md-4">
+                                <div className="p-inputgroup">
+                                    <InputText placeholder="Usuario" size="34" />
+                                    <span className="p-inputgroup-addon">
+                                        <i className="pi pi-user"></i>
+                                    </span>
+                                </div>
+                            </div>
+                            <br />
+                            <div className="p-col-12 p-md-4">
+                                <div className="p-inputgroup">
+                                    <InputText type="password" placeholder="Password" size="34" />
+                                    <span className="p-inputgroup-addon">
+                                        <i className="pi pi-lock"></i>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="content-section implementation button-demo">
+                            {/* <Captcha id="captcha" size="34" language="es" theme="light" siteKey="6LeEvFsUAAAAALqEXVMvulgVYRfNdcJdJKbCq6gO" onResponse={this.showResponse} /> */}
+                        </div>
+                        <br />
+                        <Button type="submit" label="Acceder" className="p-button-raised p-button-primary" />
+                        <p>Olvidaste la contraseña? Recuperar clave</p>
+                    </form>
+                </Card>
             </div>
         );
     }
